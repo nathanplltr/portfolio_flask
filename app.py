2624553176eb0980f0fe90c4e748b2bd7ab16e1c
Nathan Pelletier <nathan.plltr@gmail.com>
2026-02-11 14:03:20 +0100

    nathan

--- app.py
+++ app.py
@@ -12,3 +12,7 @@ def about():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route('/about')
+def about():
+    return "À propos de moi : Je suis en train d'apprendre à développer des applications web avec Flask !"
