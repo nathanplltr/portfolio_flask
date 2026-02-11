b0d6203c3788d227e6cc1696608a8c1fe3862bc5
Nathan Pelletier <nathan.plltr@gmail.com>
2026-02-11 15:02:02 +0100

    Correction de la structure templates et ajout de la carte

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,14 +6,14 @@ app = Flask(__name__)
 def home():
     return "Bienvenue sur ma page d'accueil !"
 
+
 @app.route('/about')
 def about():
     return "À propos de moi : Je suis en train d'apprendre à développer des applications web avec Flask !"
 
 @app.route("/map")
-def map():
-    # C'est votre nouvelle page
+def afficher_ma_carte():  # On change 'map' en 'afficher_ma_carte'
     return render_template("map.html")
 
 if __name__ == '__main__':
-        app.run(debug=True)
+        app.run(debug=True, port=5000)
