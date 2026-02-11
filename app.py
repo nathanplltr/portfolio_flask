55596d3b668ea05656a0ce46c20c1f8fe654d8bd
Nathan Pelletier <nathan.plltr@gmail.com>
2026-02-11 15:23:21 +0100

    Modifications effectuées

--- app.py
+++ app.py
@@ -4,16 +4,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bienvenue sur ma page d'accueil !"
+    return render_template("index.html")
 
 
 @app.route('/about')
 def about():
     return "À propos de moi : Je suis en train d'apprendre à développer des applications web avec Flask !"
 
-@app.route("/map")
+@app.route("/contact")
 def afficher_ma_carte():  # On change 'map' en 'afficher_ma_carte'
-    return render_template("map.html")
+    return render_template("contact.html")
 
 if __name__ == '__main__':
         app.run(debug=True, port=5000)
